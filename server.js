633bf1c163b3d39a26968718adc5de658fd007a0
twtw <61638296+erasersleeve@users.noreply.github.com>
2020-07-08 14:41:33 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.delete("/api/notes/:id", async function (req, res){
     
 })
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
